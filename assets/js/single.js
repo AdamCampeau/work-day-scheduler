c2b8b9789c98e6c3d73330eb3a86fd1e4f3fd61f
AdamCampeau <adamcampeau3@gmail.com>
2021-11-19 20:27:54 -0500

    update row <- column <- content

--- assets/js/single.js
+++ assets/js/single.js
@@ -21,16 +21,18 @@ var currentDay=$("#currentDay");
 var todaysDate = moment().toDate();
 currentDay.text(moment(todaysDate).format("dddd, MMMM Do YYYY"));
 
-// if the time = currentHour,
-    //display current task
+
+// for 8am to 5pm
+// if the timeBlock = currentHour,
+    // display current task
     // display alert, task
     // red
 
-    // else if time < currentHour
+    // else if timeBlock < currentHour
     // display completed task
     // grey
 
-    // else time > currentHour
+    // else timeBlock > currentHour
     // display future task 
     // green
 
