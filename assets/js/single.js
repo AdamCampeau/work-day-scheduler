e4afd9043671f1b25558217e44fac993cc1a7c4e
AdamCampeau <adamcampeau3@gmail.com>
2021-11-20 22:38:14 -0500

    troubleshooting hour column

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,82 +1,26 @@
-// Trying to understand how moment.js works when compared to chapter example
-
-// The example used in the chapter is:
-
-// var californiaTime=document.querySelector("#time-california");
-// californiaTime.textContent=currentTime.format("...")
-
-//var currentDate = document.getElementById("#currentDay"); {
-    //moment();
-    //console.log()
-    //currentDay.format('DDDD, MMMM Do YYYYY')
-//}
-
-// var currentDay = moment();
-// console.log();
-// currentDay.format('DDDD, MMMM Do YYYYY')
-// );
-
+/* current date */
 
 var currentDay=$("#currentDay");
 var todaysDate = moment().toDate();
 console.log(todaysDate)
 currentDay.text(moment(todaysDate).format("dddd, MMMM Do YYYY"));
 
-
-
-// 
-// class
-// create rows as cards
+var currentHour = moment().
 // 24 hour format
-// if for loop semantics
-// if current hour, highlight current
-    //
-    
 
-var i = moment().format('h a');
+// if for loop 
 
-    for (i = 8; i < 15; i++) {
-        console.log(i)
+// i = hour (AM/PM) 
+    // for i = 8am =< 5pm, add increment of 1
 
-        // dynnamically created content
+    // dynamically generated ROWs for every hour(i)
 
-    /* ROW ORIGINAL CODE BLOCK
-    
-    <div class="row">
-        <div class ="col time-block card">
-          <div class="hour">
-            
-          </div>
-        </div>
-    
-        <div class="col-10 text-area card">
-          Task
-        </div>
-        <div class ="col">
-          <button class="saveBtn card"><i class="fas fa-save"></i>
-        </div>
-      </div>
-    */
-        var buildRow = document.createElement('row');
-       
-            var buildCard = buildRow.createElement('col');
-            buildCard.classList.add('col time-block card');
+    // when row clicked, edit textarea
 
-                var buildHour = buildCard.createElement('hour');   
-                buildHour.classList.add('hour');
-c
-            var buildText = buildRow.createElement('col-10');
-            buildText.classList.add('text-area card') 
+    // when saveBtn clicked, save to localStorage
+    // when textarea clicked, retrieve data from localStorage
 
-                // create task input
-
-            var buildCard = buildRow.createElement('col');
-            
-                var buildSave = buildCard.createElement('button');
-                button.classList.add('saveBtn card')
-                
-        
-        // if the timeBlock = currentHour,
+    // if the timeBlock = currentHour,
         // present
     
         // else if timeBlock < currentHour
@@ -84,12 +28,61 @@ c
     
         // else timeBlock > currentHour
         // future
+
+
     
-    /* i EXAMPLE
-        let str = '';
-        for (let i = 8; i < 15; i++) {
-        }*/
-    
+
+var i = moment().format('h a');
+
+    for (i = 8; i < 15; i++) {
+        console.log(i)
+
+        // ROW
+
+        var buildRow = document.createElement('row');
+       
+        var buildCardA = buildRow.createElement('col');
+        buildCard.classList.add('col time-block card');
+
+        var buildHour = buildCard.createElement('hour');   
+        buildHour.classList.add('hour');
+
+        var buildText = buildRow.createElement('col-10');
+        buildText.classList.add('textarea card') 
+
+        // create task input
+
+        var buildCardB = buildRow.createElement('col');
+            
+        var buildSave = buildCard.createElement('button');
+        button.classList.add('saveBtn card');
+                
+        /* 
+        
+        element.addEventListener("click", myFunction);
+        
+        function myFunction() {
+            alert ("Hello World!");
+        }
+        
+        */
+        
+        // store task
+        localStorage.setItem("textarea");
+        
+        // retrieve task
+        document.getElementById("edit").innerHTML = localStorage.getItem("textarea")
+
+        // save task when saveBtn clicked
+        document.getElementById("saveBtn").addEventListener("click", "textarea");
+
+        buildRow.appendChild(buildCardA)
+        buildRow.appendChild(buildHour)
+        buildRow.appendChild(buildText)
+        buildRow.appendChild(buildCardB)
+        buildRow.appendChild(buildSave)
+
+
         if (currentHour == i) {
             present;
             
@@ -108,15 +101,20 @@ c
    
 
 
-
-// event.listener "click", timeBlock
-    // text area
-    // save button, save timeBlock
-
-// save data to local storage
-     
-
-
-//var currentHour = moment().toDate();
-// currentDay.text(moment(tofd))
-
+    /* ROW ORIGINAL CODE BLOCK
+    
+    <div class="row">
+        <div class ="col time-block card">
+          <div class="hour">
+            
+          </div>
+        </div>
+    
+        <div class="col-10 text-area card">
+          Task
+        </div>
+        <div class ="col">
+          <button class="saveBtn card"><i class="fas fa-save"></i>
+        </div>
+      </div>
+    */
